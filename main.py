0ce7dcd2f251050b27906a488299461bdbceb95a
Matthew Poonoosamy <MatthewSundrasen.Poonoosamy@calbaptist.edu>
2025-01-31 23:40:51 -0800

    FriendlyInteraction in process

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, random
+import os, random, math
 
 run = True
 menu = True
@@ -20,15 +20,23 @@ gold = 0
 x = 0
 y = 0
 
-        #  x = 0       x = 1       x = 2       x = 3       x = 4       x = 5         x = 6
-map = [["plains",   "plains",   "plains",   "plains",   "forest", "mountain",       "cave"],    # y = 0
-       ["forest",   "forest",   "forest",   "forest",   "forest",    "hills",   "mountain"],    # y = 1
-       ["forest",   "fields",   "bridge",   "plains",    "hills",   "forest",      "hills"],    # y = 2
-       ["plains",     "shop",     "town",    "mayor",   "plains",    "hills",       "shop"],    # y = 3
-       ["plains",   "fields",   "fields",   "plains",    "hills", "mountain",   "mountain"]]    # y = 4
+Limiter = False
+PreviousUsed = "1 "
+currentLocX = 2
+currentLocY = 3
+currentCoords = [currentLocX,currentLocY]
+townCoords = [2,3]
+directionList = ["north","northeast","east","southeast","south","southwest","west","northwest"]
 
-y_len = len(map)-1
-x_len = len(map[0])-1
+#  x = 0       x = 1       x = 2       x = 3       x = 4       x = 5         x = 6
+map = [["plains", "plains", "plains", "plains", "forest", "mountain", "cave"],  # y = 0
+       ["forest", "forest", "forest", "forest", "forest", "hills", "mountain"],  # y = 1
+       ["forest", "fields", "bridge", "plains", "hills", "forest", "hills"],  # y = 2
+       ["plains", "shop", "town", "mayor", "plains", "hills", "shop"],  # y = 3
+       ["plains", "fields", "fields", "plains", "hills", "mountain", "mountain"]]  # y = 4
+
+y_len = len(map) - 1
+x_len = len(map[0]) - 1
 
 biom = {
     "plains": {
@@ -65,6 +73,10 @@ biom = {
 }
 
 e_list = ["Goblin", "Orc", "Slime"]
+e_listUncommon = ["Kobold", "Hobgoblin"]
+e_listRare = ["Owlin", "Dark Elf"]
+e_listEpic = ["Goliath", "Minotaur"]
+e_listFriendlys = ["Satyr", "Human", "Gnome"]
 
 mobs = {
     "Goblin": {
@@ -82,12 +94,58 @@ mobs = {
         "at": 2,
         "go": 12
     },
+    "Kobold": {
+        "hp": 30,
+        "at": 7,
+        "go": 22
+    },
+    "Hobgoblin": {
+        "hp": 45,
+        "at": 5,
+        "go": 25
+    },
+    "Owlin": {
+        "hp": 10,
+        "at": 10,
+        "go": 30
+    },
+    "Dark Elf": {
+        "hp": 30,
+        "at": 7,
+        "go": 27
+    },
+    "Goliath": {
+        "hp": 50,
+        "at": 5,
+        "go": 35
+    },
+    "Minotaur": {
+        "hp": 50,
+        "at": 5,
+        "go": 35
+    },
     "Dragon": {
         "hp": 100,
         "at": 8,
         "go": 100
+    },
+    "Satyr": {
+        "hp": 35,
+        "at": 3,
+        "go": 0
+    },
+    "Human": {
+        "hp": 15,
+        "at": 2,
+        "go": 0
+    },
+    "Gnome": {
+        "hp": 20,
+        "at": 5,
+        "go": 5
     }
 }
+
 
 def art1():
     print("""
@@ -145,6 +203,7 @@ def art1():
 @@@@@@}@@@@%@@@#}[#}@@@@@@@@@@%#}{{{{}@@@@@@@@@@@@@@@@@@@@@@@@{}{{%@@@@@@@@@@[%[{%%@@%@@@@}@@@@@@@@@
     """)
 
+
 def clear():
     print(100 * "\n")
 
@@ -181,12 +240,22 @@ def heal(amount):
         HP = HPMAX
     print(name + "'s HP refilled to " + str(HP) + "!")
 
-
 def battle():
     global fight, play, run, HP, pot, elix, gold, boss
 
     if not boss:
-        enemy = random.choice(e_list)
+        random_int = random.randint(1, 1000)
+        if random_int < 25:
+            enemy = random.choice(e_listEpic)
+        elif random_int in range(25, 150):
+            enemy = random.choice(e_listRare)
+        elif random_int in range(150, 400):
+            enemy = random.choice(e_listUncommon)
+        else:
+            if random_int > 400 and random_int % 7 == 0:
+                enemy = random.choice(e_listFriendlys)
+            else:
+                enemy = random.choice(e_list)
     else:
         enemy = "Dragon"
     hp = mobs[enemy]["hp"]
@@ -194,86 +263,136 @@ def battle():
     atk = mobs[enemy]["at"]
     g = mobs[enemy]["go"]
 
+    friend = False
+    if enemy != ["Satyr", "Human", "Gnome"]:
+        friend = True
+
     while fight:
         clear()
         draw()
-        print("Defeat the " + enemy + "!")
-        draw()
-        print(enemy + "'s HP: " + str(hp) + "/" + str(hpmax))
-        print(name + "'s HP: " + str(HP) + "/" + str(HPMAX))
-        print("POTIONS: " + str(pot))
-        print("ELIXIR: " + str(elix))
-        draw()
-        print("1 - ATTACK")
+        if not friend:
+            print("Defeat the " + enemy + "!")
+            draw();
+            print(enemy + "'s HP: " + str(hp) + "/" + str(hpmax))
+            print(name + "'s HP: " + str(HP) + "/" + str(HPMAX))
+            print("POTIONS: " + str(pot))
+            print("ELIXIR: " + str(elix))
+            draw()
+            print("1 - ATTACK")
         if pot > 0:
             print("2 - USE POTION (30HP)")
         if elix > 0:
             print("3 - USE ELIXIR (50HP)")
         draw()
 
+    else:
+        print("A " + enemy + " approaches you")
+        draw()
         choice = input("# ")
+        if enemy == "Satyr":
+            print("These woods are not safe for your kind\n1: \"I must push forward\"\n2: Turn Back\n"+"3: Attack!")
+            if choice == "1":
+                potionsgiven = random.randint(1, 4)
+                print("If you must, take these:\n+"+str(potionsgiven)+" Potions");
+                pot += potionsgiven
+            elif choice == "2":
+                Limiter = True
+                Fight = False
+            elif choice == "3":
+                friend = False
+        elif enemy == "Human":
+            print("Thank goodness you're here!\n1: \"Who are you?\"\n2: \"Empty your pockets or I'll gut you like a fish\"")
+            if choice == "1":
+                distance = str(math.dist(currentCoords,townCoords))
+                direction = directionList[1]
+                if currentLocY-3 == 0:
+                    if currentLocX-2>0:
+                        direction = directionList[3]
+                    else:
+                        direction = directionList[7]
+                elif currentLocX-2 == 0:
+                    if currentLocY-3>0:
+                        direction = directionList[5]
+                    else:
+                        direction = directionList[1]
+                elif currentLocY-3>0:
+                    if currentLocX-2>0:
+                        direction = directionList[4]
+                    else:
+                        direction = directionList[6]
+                else:
+                    if currentLocX-2>0:
+                        direction = directionList[2]
+                    else:
+                        direction = directionList[8]
 
-        if choice == "1":
-            hp -= ATK
-            print(name + " dealt " + str(ATK) + " damage to the " + enemy + ".")
-            if hp > 0:
-                HP -= atk
-                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
-            input("> ")
+                print("My name is Bartholomew Donaldson, I had too much mead last night and now I can't find the town!\n1: \"I'll help you, for a price\n2: The town is "+distance+" miles "+direction+"\n3: \"I'll take you there\"\n4: Fight!")
+        elif enemy == "Gnome":
+            print("...")
 
-        elif choice == "2":
-            if pot > 0:
-                pot -= 1
-                heal(30)
-                HP -= atk
-                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
-            else:
-                print("No potions!")
-            input("> ")
+    choice = input("# ")
 
-        elif choice == "3":
-            if elix > 0:
-                elix -= 1
-                heal(50)
-                HP -= atk
-                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
-            else:
-                print("No elixirs!")
-            input("> ")
+    if choice == "1":
+        hp -= ATK
+        print(name + " dealt " + str(ATK) + " damage to the " + enemy + ".")
+        if hp > 0:
+            HP -= atk
+            print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+        input("> ")
 
-        if HP <= 0:
-            print(enemy + " defeated " + name + "...")
+    elif choice == "2":
+        if pot > 0:
+            pot -= 1
+            heal(30)
+            HP -= atk
+            print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+        else:
+            print("No potions!")
+        input("> ")
+
+    elif choice == "3":
+        if elix > 0:
+            elix -= 1
+            heal(50)
+            HP -= atk
+            print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+        else:
+            print("No elixirs!")
+        input("> ")
+
+    if HP <= 0:
+        print(enemy + " defeated " + name + "...")
+        draw()
+        fight = False
+        play = False
+        run = False
+        print("GAME OVER")
+        input("> ")
+
+    if hp <= 0:
+        print(name + " defeated the " + enemy + "!")
+        draw()
+        fight = False
+        gold += g
+        print("You've found " + str(g) + " gold!")
+        if random.randint(0, 100) < 30:
+            pot += 1
+            print("You've found a potion!")
+        if enemy == "Dragon":
+            clear()
             draw()
-            fight = False
+            art1()
+            print("=======================================")
+            print("=        CONGRATULATIONS!!!           =")
+            print("==You've just beaten this awesome RPG!=")
+            print("=======================================")
+            print("==Please give me a star on GitHub======")
+            print("=======================================")
+            draw()
+            boss = False
             play = False
             run = False
-            print("GAME OVER")
-            input("> ")
-
-        if hp <= 0:
-            print(name + " defeated the " + enemy + "!")
-            draw()
-            fight = False
-            gold += g
-            print("You've found " + str(g) + " gold!")
-            if random.randint(0, 100) < 30:
-                pot += 1
-                print("You've found a potion!")
-            if enemy == "Dragon":
-                clear()
-                draw()
-                art1()
-                print("=======================================")
-                print("=        CONGRATULATIONS!!!           =")
-                print("==You've just beaten this awesome RPG!=")
-                print("=======================================")
-                print("==Please give me a star on GitHub======")
-                print("=======================================")
-                draw()
-                boss = False
-                play = False
-                run = False
-            input("> ")
+        input("> ")
 
 
 def shop():
@@ -372,7 +491,6 @@ def cave():
             boss = False
 
 
-
 while run:
     while menu:
         print("1, NEW GAME")
@@ -386,20 +504,22 @@ while run:
             print("Rules!")
             draw()
             print("1. Use the Number Keys to Determine Where to Go!")
-            print("2. Be sure to not stray off too far in the early game, the end of the map is where the Orcs spawn the most!")
-            print("3. Don't spam the buttons! This could not only damage the scripts, but also your internal machine, we don't want that!")
+            print(
+                "2. Be sure to not stray off too far in the early game, the end of the map is where the Orcs spawn the most!")
+            print(
+                "3. Don't spam the buttons! This could not only damage the scripts, but also your internal machine, we don't want that!")
             draw()
             print("Speedrunning!")
             draw()
             print("- The timer starts when you hit enter on the name select screen.")
             print("- The timer ends upon the last hit of fighting the dragon.")
-            print("The current World Record is 1:45, can you beat it?!")    
+            print("The current World Record is 1:25.48, can you beat it?!")
             draw()
             print("Press Enter to Continue...")
             rules = False
             choice = ""
             input("> ")
-            clear()    
+            clear()
         else:
             choice = input("# ")
 
@@ -461,60 +581,68 @@ while run:
             print("COORD:", x, y)
             draw()
             print("0 - SAVE AND QUIT")
-            if y > 0:
-                print("1 - NORTH")
-            if x < x_len:
-                print("2 - EAST")
-            if y < y_len:
-                print("3 - SOUTH")
-            if x > 0:
-                print("4 - WEST")
-            if pot > 0:
-                print("5 - USE POTION (30HP)")
-            if elix > 0:
-                print("6 - USE ELIXIR (50HP)")
-            if map[y][x] == "shop" or map[y][x] == "mayor" or map[y][x] == "cave":
-                print("7 - ENTER")
-            draw()
-
             dest = input("# ")
+
+            if Limiter:
+                if y > 0 and previousUsed != "1":
+                    print("1 - NORTH")
+                if x < x_len and previousUsed != "2":
+                    print("2 - EAST")
+                if y < y_len and previousUsed != "3":
+                    print("3 - SOUTH")
+                if x > 0 and previousUsed != "4":
+                    print("4 - WEST")
+                if pot > 0:
+                    print("5 - USE POTION (30HP)")
+                if elix > 0:
+                    print("6 - USE ELIXIR (50HP)")
+                if map[y][x] == "shop" or map[y][x] == "mayor" or map[y][x] == "cave":
+                    print("7 - ENTER")
 
             if dest == "0":
                 play = False
                 menu = True
                 save()
-            elif dest == "1":
+            elif dest == "1" and previousUsed != "1":
                 if y > 0:
                     y -= 1
-                    standing = False
-            elif dest == "2":
+                standing = False
+                currentLocY += 1
+                previousUsed = 1
+            elif dest == "2" and previousUsed != "2":
                 if x < x_len:
                     x += 1
-                    standing = False
-            elif dest == "3":
+                standing = False
+                currentLocX += 1
+                previousUsed = 2
+            elif dest == "3" and previousUsed != "3":
                 if y < y_len:
                     y += 1
-                    standing = False
-            elif dest == "4":
+                standing = False
+                currentLocY -= 1
+                previousUsed = 3
+            elif dest == "4" and previousUsed != "1":
                 if x > 0:
                     x -= 1
-                    standing = False
+                standing = False
+                currentLocX -= 1
+                previousUsed = 4
             elif dest == "5":
                 if pot > 0:
                     pot -= 1
                     heal(30)
                 else:
                     print("No potions!")
-                input("> ")
-                standing = True
+                    input("> ")
+                    standing = True
             elif dest == "6":
                 if elix > 0:
                     elix -= 1
                     heal(50)
                 else:
                     print("No elixirs!")
-                input("> ")
-                standing = True
+                    input("> ")
+                    standing = True
             elif dest == "7":
                 if map[y][x] == "shop":
                     buy = True
@@ -525,5 +653,74 @@ while run:
                 if map[y][x] == "cave":
                     boss = True
                     cave()
+                else:
+                    standing = True
             else:
-                standing = True
+                if y > 0:
+                    print("1 - NORTH")
+                if x < x_len:
+                    print("2 - EAST")
+                if y < y_len:
+                    print("3 - SOUTH")
+                if x > 0:
+                    print("4 - WEST")
+                if pot > 0:
+                    print("5 - USE POTION (30HP)")
+                if elix > 0:
+                    print("6 - USE ELIXIR (50HP)")
+                if map[y][x] == "shop" or map[y][x] == "mayor" or map[y][x] == "cave":
+                    print("7 - ENTER")
+                draw()
+
+                if dest == "0":
+                    play = False
+                    menu = True
+                    save()
+                elif dest == "1":
+                    if y > 0:
+                        y -= 1
+                    standing = False
+                    currentLocY += 1
+                elif dest == "2":
+                    if x < x_len:
+                        x += 1
+                    standing = False
+                    currentLocX += 1
+                elif dest == "3":
+                    if y < y_len:
+                        y += 1
+                    standing = False
+                    currentLocY -= 1
+                elif dest == "4":
+                    if x > 0:
+                        x -= 1
+                    standing = False
+                    currentLocX -= 1
+                elif dest == "5":
+                    if pot > 0:
+                        pot -= 1
+                        heal(30)
+                    else:
+                        print("No potions!")
+                        input("> ")
+                        standing = True
+                elif dest == "6":
+                    if elix > 0:
+                        elix -= 1
+                        heal(50)
+                    else:
+                        print("No elixirs!")
+                        input("> ")
+                        standing = True
+                elif dest == "7":
+                    if map[y][x] == "shop":
+                        buy = True
+                    shop()
+                if map[y][x] == "mayor":
+                    speak = True
+                    mayor()
+                if map[y][x] == "cave":
+                    boss = True
+                    cave()
+                else:
+                    standing = True
